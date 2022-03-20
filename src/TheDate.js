d3a989bf6cfa2d131eac8444455de3c6d81a1b53
mercy-wumi <m.adams1909@gmail.com>
2022-03-20 01:02:28 +0100

    completed get date app using class component

--- src/TheDate.js
+++ src/TheDate.js
@@ -1,12 +1,28 @@
 import React from 'react';
 
 class TheDate extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
+    this.state = {
+      date: new Date(),
+      showDate: false
     }
-    render() {
-      return <div />
-    }
+    this.handleShowDate = this.handleShowDate.bind(this)
+  }
+  handleShowDate = () => {
+    this.setState({
+      showDate: true
+    })
+  }
+
+  render() {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', marginTop: '20%', textAlign: 'center', padding: '0px 20px' }}>
+        <p>{this.state.showDate ? this.state.date.toString() : ''}</p>
+        <button onClick={this.handleShowDate}>Show Date</button>
+      </div>
+    )
+  }
 }
 
 export default TheDate
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
+import TheDate from './TheDate'
 import Accordion from './state-drills/Accordion'
 
 ReactDOM.render(
-  
-    <Accordion sections={[{title:'title1', content:'content1'}, {title:'title2', content:'content2'}]}/>
-    ,
+
+  // <Accordion sections={[{title:'title1', content:'content1'}, {title:'title2', content:'content2'}]}/>
+  <TheDate />,
   document.getElementById('root')
 )
